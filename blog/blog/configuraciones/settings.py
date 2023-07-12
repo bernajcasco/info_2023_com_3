05f047c81e83f07a2eeccfcd6acb0c28017f5686
bernajcasco <berna.casco@gmail.com>
2023-07-12 20:13:35 -0300

    nuevo models

--- blog/blog/configuraciones/settings.py
+++ blog/blog/configuraciones/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5%-4(yxttufvbm-y)kesl(%@#sqi6b06ev)m(d-#eqap&5@)8z'
+SECRET_KEY = 'django-insecure-2k*&8rv1io_20wh%7g$vk@@n@=u7f3dro2qz10oexn9tg$dc54'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
